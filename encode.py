7248de96cd47180e2206abf8167cde87219a2789
Jingkang Zhang <zjk@berkeley.edu>
2019-03-25 15:49:07 -0700

    re-encoded

--- encode.py
+++ encode.py
@@ -1,7 +1,9 @@
 import base64
-f = open("test_explicit.py", "rb")
+# f = open("test_explicit.py", "rb")
+f = open("puzzle.py", "rb")
 encoded = base64.b64encode(f.read())
-f = open("test.py", "wb")
+# f = open("test.py", "wb")
+f = open("demo.py", "wb")
 f.write(bytes("import base64\nmyscript ='''", 'utf-8'))
 f.write(encoded)
 f.write(bytes("'''\n", 'utf-8'))
